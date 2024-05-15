9067b624809cd744b56b78aa8835926e2993744f
ThangDevAlone <quangthangvtlg@gmail.com>
2024-05-15 11:27:28 +0700

    test dev

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -5,7 +5,9 @@ type Props = {};
 const Header = (props: Props) => {
   return (
     <header>
-      <nav className="fixed left-0 top-0 z-20 mx-auto flex h-[88px] w-full items-center border-b-4 border-black bg-white px-5 m500:h-16 "></nav>
+      <nav className="fixed left-0 top-0 z-20 mx-auto flex h-[88px] w-full items-center border-b-4 border-black bg-white px-5 m500:h-16 ">
+        ádhiasjodhaijoskdhaos
+      </nav>
     </header>
   );
 };
