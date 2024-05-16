af013f36b5821c1bc97a22304527cb696237107b
ThangDevAlone <quangthangvtlg@gmail.com>
2024-05-16 15:01:48 +0700

    hehe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="bg-[radial-gradient(#cacbce_1px,transparent_1px)] [background-size:16px_16px] ml-[250px] min-h-[100dvh] w-[full-250px] bg-bg px-5 pt-[88px] m800:ml-[180px] m800:w-[full-180px] m600:m-0 m600:w-full m500:pt-16">
-      thang hehe
+      thang ffổitoiorti
     </div>
   );
 }
