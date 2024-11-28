975058f73393edc12bd4546a83c3b3f6d88e4793
ThangDevAlone <quangthangvtlg@gmail.com>
2024-11-28 09:08:52 +0700

    :bug: fix ci

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,13 +9,13 @@ export default function Home() {
       <div
         className="mx-auto w-[700px] py-20 leading-relaxed prose-h2:mb-8 prose-headings:font-heading prose-h2:text-2xl prose-h3:mb-6 prose-h3:text-xl prose-p:leading-7 prose-p:font-base prose-a:underline prose-a:font-heading prose-code:mx-1 prose-code:rounded-base prose-code:font-bold prose-code:border prose-code:border-black prose-code:bg-main prose-code:px-2 m1000:w-[500px] m750:w-[350px] m400:w-full m400:py-16 prose-h2:m400:text-xl prose-h3:m400:text-lg">
         <h2 className="text-2xl font-semibold mb-2">
-          Giới thiệu phần mềm trò chơi "Name company"
+          Giới thiệu phần mềm trò chơi (Name company)
         </h2>
 
         <p>
           Ghé thắm{" "}
           <Link href="#" target="_blank">
-            "Name company"
+            (Name company)
           </Link>{" "}
           để biết thêm về chúng tôi.
         </p>
